48c3d4d7412e2e5e69f364f928a523c178f925ac
Jin Kim <sotdjin@gmail.com>
2016-05-02 16:12:41 -0500

    re-implemented motor shield

--- u_machine2016/u_machine2016.ino
+++ u_machine2016/u_machine2016.ino
@@ -1,12 +1,112 @@
+#include <MaxMatrix.h>
 #include <Wire.h>
 #include <SparkFun_APDS9960.h>
 #include <AFMotor.h>
 #include <uspeech.h>
 #include <Servo.h>
+#include <avr/pgmspace.h>
 
 Servo arm_servo, door_servo;
 
 #define APDS9960_INT    2 // Needs to be an interrupt pin
+
+PROGMEM const unsigned char CH[] = {
+  3, 8, B00000000, B00000000, B00000000, B00000000, B00000000, // space
+  1, 8, B01011111, B00000000, B00000000, B00000000, B00000000, // !
+  3, 8, B00000011, B00000000, B00000011, B00000000, B00000000, // "
+  5, 8, B00010100, B00111110, B00010100, B00111110, B00010100, // #
+  4, 8, B00100100, B01101010, B00101011, B00010010, B00000000, // $
+  5, 8, B01100011, B00010011, B00001000, B01100100, B01100011, // %
+  5, 8, B00110110, B01001001, B01010110, B00100000, B01010000, // &
+  1, 8, B00000011, B00000000, B00000000, B00000000, B00000000, // '
+  3, 8, B00011100, B00100010, B01000001, B00000000, B00000000, // (
+  3, 8, B01000001, B00100010, B00011100, B00000000, B00000000, // )
+  5, 8, B00101000, B00011000, B00001110, B00011000, B00101000, // *
+  5, 8, B00001000, B00001000, B00111110, B00001000, B00001000, // +
+  2, 8, B10110000, B01110000, B00000000, B00000000, B00000000, // ,
+  4, 8, B00001000, B00001000, B00001000, B00001000, B00000000, // -
+  2, 8, B01100000, B01100000, B00000000, B00000000, B00000000, // .
+  4, 8, B01100000, B00011000, B00000110, B00000001, B00000000, // /
+  4, 8, B00111110, B01000001, B01000001, B00111110, B00000000, // 0
+  3, 8, B01000010, B01111111, B01000000, B00000000, B00000000, // 1
+  4, 8, B01100010, B01010001, B01001001, B01000110, B00000000, // 2
+  4, 8, B00100010, B01000001, B01001001, B00110110, B00000000, // 3
+  4, 8, B00011000, B00010100, B00010010, B01111111, B00000000, // 4
+  4, 8, B00100111, B01000101, B01000101, B00111001, B00000000, // 5
+  4, 8, B00111110, B01001001, B01001001, B00110000, B00000000, // 6
+  4, 8, B01100001, B00010001, B00001001, B00000111, B00000000, // 7
+  4, 8, B00110110, B01001001, B01001001, B00110110, B00000000, // 8
+  4, 8, B00000110, B01001001, B01001001, B00111110, B00000000, // 9
+  2, 8, B01010000, B00000000, B00000000, B00000000, B00000000, // :
+  2, 8, B10000000, B01010000, B00000000, B00000000, B00000000, // ;
+  3, 8, B00010000, B00101000, B01000100, B00000000, B00000000, // <
+  3, 8, B00010100, B00010100, B00010100, B00000000, B00000000, // =
+  3, 8, B01000100, B00101000, B00010000, B00000000, B00000000, // >
+  4, 8, B00000010, B01011001, B00001001, B00000110, B00000000, // ?
+  5, 8, B00111110, B01001001, B01010101, B01011101, B00001110, // @
+  4, 8, B01111110, B00010001, B00010001, B01111110, B00000000, // A
+  4, 8, B01111111, B01001001, B01001001, B00110110, B00000000, // B
+  4, 8, B00111110, B01000001, B01000001, B00100010, B00000000, // C
+  4, 8, B01111111, B01000001, B01000001, B00111110, B00000000, // D
+  4, 8, B01111111, B01001001, B01001001, B01000001, B00000000, // E
+  4, 8, B01111111, B00001001, B00001001, B00000001, B00000000, // F
+  4, 8, B00111110, B01000001, B01001001, B01111010, B00000000, // G
+  4, 8, B01111111, B00001000, B00001000, B01111111, B00000000, // H
+  3, 8, B01000001, B01111111, B01000001, B00000000, B00000000, // I
+  4, 8, B00110000, B01000000, B01000001, B00111111, B00000000, // J
+  4, 8, B01111111, B00001000, B00010100, B01100011, B00000000, // K
+  4, 8, B01111111, B01000000, B01000000, B01000000, B00000000, // L
+  5, 8, B01111111, B00000010, B00001100, B00000010, B01111111, // M
+  5, 8, B01111111, B00000100, B00001000, B00010000, B01111111, // N
+  4, 8, B00111110, B01000001, B01000001, B00111110, B00000000, // O
+  4, 8, B01111111, B00001001, B00001001, B00000110, B00000000, // P
+  4, 8, B00111110, B01000001, B01000001, B10111110, B00000000, // Q
+  4, 8, B01111111, B00001001, B00001001, B01110110, B00000000, // R
+  4, 8, B01000110, B01001001, B01001001, B00110010, B00000000, // S
+  5, 8, B00000001, B00000001, B01111111, B00000001, B00000001, // T
+  4, 8, B00111111, B01000000, B01000000, B00111111, B00000000, // U
+  5, 8, B00001111, B00110000, B01000000, B00110000, B00001111, // V
+  5, 8, B00111111, B01000000, B00111000, B01000000, B00111111, // W
+  5, 8, B01100011, B00010100, B00001000, B00010100, B01100011, // X
+  5, 8, B00000111, B00001000, B01110000, B00001000, B00000111, // Y
+  4, 8, B01100001, B01010001, B01001001, B01000111, B00000000, // Z
+  2, 8, B01111111, B01000001, B00000000, B00000000, B00000000, // [
+  4, 8, B00000001, B00000110, B00011000, B01100000, B00000000, // \ backslash
+  2, 8, B01000001, B01111111, B00000000, B00000000, B00000000, // ]
+  3, 8, B00000010, B00000001, B00000010, B00000000, B00000000, // hat
+  4, 8, B01000000, B01000000, B01000000, B01000000, B00000000, // _
+  2, 8, B00000001, B00000010, B00000000, B00000000, B00000000, // `
+  4, 8, B00100000, B01010100, B01010100, B01111000, B00000000, // a
+  4, 8, B01111111, B01000100, B01000100, B00111000, B00000000, // b
+  4, 8, B00111000, B01000100, B01000100, B00101000, B00000000, // c
+  4, 8, B00111000, B01000100, B01000100, B01111111, B00000000, // d
+  4, 8, B00111000, B01010100, B01010100, B00011000, B00000000, // e
+  3, 8, B00000100, B01111110, B00000101, B00000000, B00000000, // f
+  4, 8, B10011000, B10100100, B10100100, B01111000, B00000000, // g
+  4, 8, B01111111, B00000100, B00000100, B01111000, B00000000, // h
+  3, 8, B01000100, B01111101, B01000000, B00000000, B00000000, // i
+  4, 8, B01000000, B10000000, B10000100, B01111101, B00000000, // j
+  4, 8, B01111111, B00010000, B00101000, B01000100, B00000000, // k
+  3, 8, B01000001, B01111111, B01000000, B00000000, B00000000, // l
+  5, 8, B01111100, B00000100, B01111100, B00000100, B01111000, // m
+  4, 8, B01111100, B00000100, B00000100, B01111000, B00000000, // n
+  4, 8, B00111000, B01000100, B01000100, B00111000, B00000000, // o
+  4, 8, B11111100, B00100100, B00100100, B00011000, B00000000, // p
+  4, 8, B00011000, B00100100, B00100100, B11111100, B00000000, // q
+  4, 8, B01111100, B00001000, B00000100, B00000100, B00000000, // r
+  4, 8, B01001000, B01010100, B01010100, B00100100, B00000000, // s
+  3, 8, B00000100, B00111111, B01000100, B00000000, B00000000, // t
+  4, 8, B00111100, B01000000, B01000000, B01111100, B00000000, // u
+  5, 8, B00011100, B00100000, B01000000, B00100000, B00011100, // v
+  5, 8, B00111100, B01000000, B00111100, B01000000, B00111100, // w
+  5, 8, B01000100, B00101000, B00010000, B00101000, B01000100, // x
+  4, 8, B10011100, B10100000, B10100000, B01111100, B00000000, // y
+  3, 8, B01100100, B01010100, B01001100, B00000000, B00000000, // z
+  3, 8, B00001000, B00110110, B01000001, B00000000, B00000000, // {
+  1, 8, B01111111, B00000000, B00000000, B00000000, B00000000, // |
+  3, 8, B01000001, B00110110, B00001000, B00000000, B00000000, // }
+  4, 8, B00001000, B00000100, B00001000, B00000100, B00000000, // ~
+};
 
 // VARIABLES
 SparkFun_APDS9960 apds = SparkFun_APDS9960();
@@ -20,17 +120,24 @@ int val = 0;
 bool mode_input = false;
 bool move_back = false;
 bool door_open = false;
+bool intro_done = false;
 
 // PINS
 int switch_pin = 11;
 int arm_pin = 8;
 int door_pin = 7;
-int led_pin = 13;
 int pir_pin = 4;
+int data = 6;    // DIN pin of MAX7219 module
+int load = 13;    // CS pin of MAX7219 module
+int clock = 10;  // CLK pin of MAX7219 module*/
 int motor_direction = 12;
-int motor_brake = 9;
 int motor_pwm = 3;
+int motor_brake = 9;
 signal voice(A0);
+
+int maxInUse = 2;  //how many MAX7219 are connected
+MaxMatrix m(data, load, clock, maxInUse); // define Library
+byte buffer[10];
 
 void setup() {
   // Voice Coeff and Calibrate;
@@ -53,11 +160,10 @@ void setup() {
   pinMode(motor_direction, OUTPUT);
   pinMode(motor_brake, OUTPUT);
   pinMode(APDS9960_INT, INPUT);
-  pinMode(led_pin, OUTPUT);
-
+  
   // Random and Serial start
-  randomSeed(analogRead(1));
   Serial.begin(9600);
+  randomSeed(analogRead(1));
   delay(10);
   Serial.println(F("--------------------------------"));
   Serial.println(F("SparkFun APDS-9960 - Gesture"));
@@ -81,10 +187,16 @@ void setup() {
   else {
     Serial.println(F("Something went wrong during gesture sensor init!"));
   }
+
+  m.init();
+  m.setIntensity(5);
 }
 
 void loop() {
-  //LED CODE HERE?
+  if (!intro_done) {
+    introduction();
+  }
+  
   char p = voice.getPhoneme();
   Serial.println(p);
   if (p != ' ') {
@@ -101,6 +213,11 @@ void loop() {
     }
     else {
       Serial.println("Switch_Mode");
+      char string2[] = "Switch Mode   ";  // Scrolling Text
+      delay(100);
+      m.shiftLeft(false, true);
+      printStringWithShift(string2, 100);
+      
       rand_num = random(3);
       int switch_state = digitalRead(switch_pin);
       if (switch_state == HIGH) {
@@ -121,8 +238,20 @@ void loop() {
   }
 }
 
+void introduction() {
+  char string1[] = "LMA BOX   ";  // Scrolling Text
+  delay(100);
+  m.shiftLeft(false, true);
+  printStringWithShift(string1, 100);
+  intro_done = true;
+}
 void move_away_mode() {
   Serial.println("move_away_mode()");
+  char string3[] = "Move Away   ";  // Scrolling Text
+  delay(100);
+  m.shiftLeft(false, true);
+  printStringWithShift(string3, 100);
+  
   bool return_main = false;
   int f_or_b;
   while (!return_main) {
@@ -142,8 +271,6 @@ void move_away_mode() {
     }
     else {
       if (!return_main && f_or_b == 0) {
-        Serial.println("Here_move_away");
-        // LED DISPLAY?
         if (isr_flag == 1) {
           detachInterrupt(0);
           handleGesture();
@@ -154,14 +281,14 @@ void move_away_mode() {
         if (val == 1) {
           digitalWrite(motor_direction, LOW);
           digitalWrite(motor_brake, LOW);
-          analogWrite(motor_pwm, 150);
+          analogWrite(motor_pwm, 200);
           count++;
-          delay(200);
-          digitalWrite(motor_brake, HIGH);
+          delay(1500);
+          //digitalWrite(motor_brake, HIGH);
           val = 0;
         }
         else {
-          digitalWrite(motor_brake, HIGH);
+          //digitalWrite(motor_brake, HIGH);
         }
       }
       else if (!return_main && f_or_b == 1) {
@@ -170,18 +297,21 @@ void move_away_mode() {
     }
   }
 }
-
 void move_back_mode() {
-  Serial.println("Here_move_back");
-  // LED DISPLAY?
+  Serial.println("move_back_mode()");
+  char string4[] = "Move Back   ";  // Scrolling Text
+  delay(100);
+  m.shiftLeft(false, true);
+  printStringWithShift(string4, 100);
+  
   digitalWrite(motor_direction, HIGH);
   digitalWrite(motor_brake, LOW);
   analogWrite(motor_pwm, 150);
-  delay(200 * count);
+  delay(1500 * count);
   digitalWrite(motor_brake, HIGH);
-  move_back = false;
   count = 0;
 }
+
 void interruptRoutine() {
   isr_flag = 1;
 }
@@ -342,5 +472,25 @@ void captain_crazy() {
   for (pos_door = 70; pos_door > 5; pos_door -= 15) {
     door_servo.write(pos_door);
     delay(15);
+  }
+}
+void printCharWithShift(char c, int shift_speed) {
+  if (c < 32) return;
+  c -= 32;
+  memcpy_P(buffer, CH + 7*c, 7);
+  m.writeSprite(maxInUse*8, 0, buffer);
+  m.setColumn(maxInUse*8 + buffer[0], 0);
+  
+  for (int i=0; i<buffer[0]+1; i++) 
+  {
+    delay(shift_speed);
+    m.shiftLeft(false, false);
+  }
+}
+// Extract characters from Scrolling text
+void printStringWithShift(char* s, int shift_speed){
+  while (*s != 0){
+    printCharWithShift(*s, shift_speed);
+    s++;
   }
 }
